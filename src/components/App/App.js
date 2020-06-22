6684b54f5c27b266c0dfc1b92c39df8644fe0381
juanmesa2097 <juanmesa2097@gmail.com>
2020-06-21 23:14:31 -0500

    Make requests for places

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,80 @@
 import React, { Component } from "react";
 import Toolbar from "../Toolbar/Toolbar";
 import "./App.css";
-import MapContainer from "../MapContainer/MapContainer";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.inputSearchRef = React.createRef();
+    this.state = { map: "" };
+    this.service = undefined;
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
-  async handleSearch(e) {
-    console.log(e);
+  componentDidMount() {
+    const googleMapsApiLoaded = setInterval(() => {
+      if (window.google) {
+        clearInterval(googleMapsApiLoaded);
 
-    const service = window.google.maps.places.PlacesService;
-    //  service.find
+        var sydney = new window.google.maps.LatLng(-33.867, 151.195);
+
+        this.map = new window.google.maps.Map(document.getElementById("map"), {
+          center: sydney,
+          zoom: 15,
+        });
+
+        this.service = new window.google.maps.places.PlacesService(this.map);
+      }
+    }, 100);
+  }
+
+  handleSearch(query) {
+    const request = {
+      query,
+      fields: ["formatted_address", "name", "place_id"],
+    };
+
+    this.service.findPlaceFromQuery(request, this.handleMainResult.bind(this));
+  }
+
+  handleMainResult(results, status) {
+    if (status === "OK") {
+      console.log(results);
+      results.forEach((r) => {
+        const detailsRequest = {
+          placeId: r.place_id,
+          fields: [
+            "name",
+            "rating",
+            "formatted_phone_number",
+            "geometry",
+            "photos",
+          ],
+        };
+
+        this.service.getDetails(
+          detailsRequest,
+          this.handleDetailsResult.bind(this)
+        );
+      });
+    }
+  }
+
+  handleDetailsResult(results, status) {
+    if (status === "OK") {
+      const lat = results.geometry.location.lat();
+      const lng = results.geometry.location.lng();
+      this.refreshMapLocation(lat, lng);
+      console.log(results);
+    }
+  }
+
+  refreshMapLocation(lat, lng) {
+    this.setState({
+      map: new window.google.maps.Map(document.getElementById("map"), {
+        center: new window.google.maps.LatLng(lat, lng),
+        zoom: 15,
+      }),
+    });
   }
 
   render() {
@@ -21,7 +82,7 @@ export default class App extends Component {
       <div className="container-fluid pl-0">
         <div className="row align-items-center">
           <div className="App-col col-12 col-md-6">
-            <MapContainer></MapContainer>
+            <div id="map"></div>
           </div>
           <div className="App-col col-12 col-md-6">
             <Toolbar onSearch={this.handleSearch}></Toolbar>
